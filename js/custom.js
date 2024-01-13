20958df5d4e4b6ced6a29b2ff9e815f8c867ddb7
zbidiHassen <150421850+zbidiHassen@users.noreply.github.com>
2024-01-13 20:45:21 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+    
+}
+
+
+
+function done(){
+    if (document.getElementById("email").value=== '')
+    alert("Enter your email ");
+    
+    else 
+    alert("Done !")
+}
+
